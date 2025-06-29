2835e3070c5f888eba506e9aa79dc28dbf30207d
celia qiang <dadenature@gmail.com>
2025-06-29 11:11:57 +0100

    TradeSystem: Refactor types.h
    Rename variables
    
    For better UnderStanding and clarity
    
    Signed-off-by: celia qiang <dadenature@gmail.com>

--- types.h
+++ types.h
@@ -11,7 +11,7 @@ enum class ActionType
     HOLD
 };
 
-enum class SymoblType
+enum class SymbolType
 {
     TEST_SYMBOL, // Example symbol
     // TODO: Add more symbol types as needed
@@ -20,13 +20,13 @@ enum class SymoblType
 struct TradeData
 {
     double price_;
-    long long timestamp_ms_; 
+    long long timestamp_ms_;
 
     TradeData(double price) : price_(price)
     {
         timestamp_ms_ = std::chrono::duration_cast<std::chrono::milliseconds>(
-                            std::chrono::system_clock::now().time_since_epoch())
-                            .count();
+                                    std::chrono::system_clock::now().time_since_epoch())
+                                    .count();
     }
     TradeData() : price_(0.0), timestamp_ms_(0) {}
 };
@@ -42,13 +42,13 @@ struct ActionSignal
         : type_(type), price_(price), amount_(amount)
     {
         timestamp_ms_ = std::chrono::duration_cast<std::chrono::milliseconds>(
-                            std::chrono::system_clock::now().time_since_epoch())
-                            .count();
+                                    std::chrono::system_clock::now().time_since_epoch())
+                                    .count();
     }
     ActionSignal() : type_(ActionType::HOLD), price_(0.0), amount_(0.0), timestamp_ms_(0) {}
 };
 
-using doubleVect = std::vector<double>;
-using dataVect = std::vector<TradeData>;
+using DoubleVector = std::vector<double>;
+using TradeDataVector = std::vector<TradeData>;
 
 #endif // TYPES_H
