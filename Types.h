c100c6661568033c44ef8042bca2a69ab5b3e832
celia qiang <celia.qiang@qualitrolcorp.com>
2025-07-05 23:26:10 +0100

    TradeSystem: Add a user type for ParameterCheck
    For test
    
    Signed-off-by: celia qiang <celia.qiang@qualitrolcorp.com>

--- Types.h
+++ Types.h
@@ -51,4 +51,27 @@ struct ActionSignal
 using DoubleVector = std::vector<double>;
 using TradeDataVector = std::vector<TradeData>;
 
+
+//  More types can be added for checking 
+/**
+ * @class IntRange
+ * @brief This class is specially used for parameter checking.
+ *
+ * It represents an integer range with a value `x` and boundaries `min` and `max`.
+ * The class provides a method to validate whether `x` lies within the range.
+ */
+class IntRange {
+public:
+    int x;
+    int min;
+    int max;
+
+    IntRange(int value = 0, int minimum = 0, int maximum = 0)
+        : x(value), min(minimum), max(maximum) {}
+
+    bool isValid() const {
+        return x >= min && x <= max;
+    }
+};
+
 #endif // TYPES_H
